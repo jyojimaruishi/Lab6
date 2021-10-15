8e08955c582843997a5ee4b141379dc208c19498
jyojimaruishi <89753957+jyojimaruishi@users.noreply.github.com>
2021-10-15 19:54:39 +0000

    uncapitalized parameters to make sentences look natural

--- Main.java
+++ Main.java
@@ -1,13 +1,13 @@
 class Main {
   public static void main(String[] args) {
     Tree tree1 = new Tree();
-    Tree tree2 = new Tree("Maple", "Broadleaf", true, "Red");
+    Tree tree2 = new Tree("maple", "broadleaf", true, "red");
 
     tree1.print();
     tree2.print();
 
-    tree1.setName("Spruce");
-    tree1.setType("Conifer");
+    tree1.setName("spruce");
+    tree1.setType("conifer");
     tree1.print();
   }
 }
